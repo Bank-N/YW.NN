bf69fadbb23417722908186839eef0e4edb73737
Yotsawin Naknam <73086330+Bank-N@users.noreply.github.com>
2021-11-05 09:44:40 +0700

    Update CPF data and picture.py

--- CPF data and picture.py
+++ CPF data and picture.py
@@ -1,20 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 2,
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "SyntaxError",
-     "evalue": "invalid syntax (<ipython-input-2-8f4797b70dce>, line 17)",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;36m  File \u001b[1;32m\"<ipython-input-2-8f4797b70dce>\"\u001b[1;36m, line \u001b[1;32m17\u001b[0m\n\u001b[1;33m    \"NameFlag\" : [\"['NOVA', 'MOXIN', '800']\",\"['NEO','NOVA-500']\",\u001b[0m\n\u001b[1;37m    ^\u001b[0m\n\u001b[1;31mSyntaxError\u001b[0m\u001b[1;31m:\u001b[0m invalid syntax\n"
-     ]
-    }
-   ],
-   "source": [
     "import pandas as pd\n",
     "import cv2\n",
     "import pytesseract as tess\n",
@@ -39,31 +22,4 @@
     "\n",
     "#อย่าลืมเปลี่ยน path\n",
     "export_csv = daraFrame.to_csv(r'E:\\BANK\\Studies\\Programming Fundamentals\\Dek 64\\CPF Projects\\Medicine_Data.csv', index = False, header = True)"
-   ]
-  }
- ],
- "metadata": {
-  "interpreter": {
-   "hash": "b3ba2566441a7c06988d0923437866b63cedc61552a5af99d1f4fb67d367b25f"
-  },
-  "kernelspec": {
-   "display_name": "Python 3.8.8 64-bit ('base': conda)",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.8.8"
-  },
-  "orig_nbformat": 4
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+ 
